dc671b5a95ba5eaaeac3c4fcf0915787636ff35b
sproutseeds <codyshanemitchell@gmail.com>
2020-04-17 13:49:12 -0700

    Added the Clarifai API, a div container to display the image we are linking.

--- face_recognition_brain/src/App.js
+++ face_recognition_brain/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import Clarifai from "clarifai";
+import Particles from "react-particles-js";
+
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
-import Particles from "react-particles-js";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "./App.css";
 import "tachyons";
+
+const app = new Clarifai.App({
+  apiKey: "1bcf1a767322480c9a22197063375b4f",
+});
 
 const particleOptions = {
   particles: {
@@ -22,6 +29,31 @@ const particleOptions = {
   },
 };
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+  onButtonSubmit = (event) => {
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        // do something with response
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function (err) {
+        // there was an error
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
@@ -29,10 +61,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* {
-      
-      <FaceRecognition />} */}
+        <ImageLinkForm
+          onButtonSubmit={this.onButtonSubmit}
+          onInputChange={this.onInputChange}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
---
+++ face_recognition_brain/src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./FaceRecognition.css";
+
+const FaceRecognition = ({ imageUrl }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img alt="picture" src={imageUrl} width="500px" height="auto"></img>
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
