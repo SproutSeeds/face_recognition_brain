c3fe0499cc68a412183d4c5630e320a9f06e25e2
sproutseeds <codyshanemitchell@gmail.com>
2020-04-17 12:56:36 -0700

    Front End Design of Face Recognition App

---
+++ face_recognition_brain/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+import "./App.css";
+import "tachyons";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    size: {
+      value: 1,
+    },
+  },
+};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particleOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* {
+      
+      <FaceRecognition />} */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ face_recognition_brain/src/components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <p
+        className="f3 link dim black underline pa3 pointer "
+        style={{ display: "flex", justifyContent: "flex-end" }}
+      >
+        Sign Out
+      </p>
+    </nav>
+  );
+};
+
+export default Navigation;
